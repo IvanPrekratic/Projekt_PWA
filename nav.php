0956b5b79f4c127017d42f82e30c6750b1e8581c
IvanPrekratic <ivan.prekratic1@gmail.com>
2023-06-24 11:34:14 +0200

    dijakritika

--- nav.php
+++ nav.php
@@ -7,7 +7,7 @@
             <li><a href="kategorija.php?kategorija=Politika">POLITIKA</a></li>
             <li><a href="kategorija.php?kategorija=Sport">SPORT</a></li>
             <li><a href="administracija.php">ADMINISTRACIJA</a></li>
-            <li><a href="unos.php">DODAVANJE CLANKA</a></li>
+            <li><a href="unos.php">DODAVANJE ČLANKA</a></li>
             '; if (isset($_SESSION['username']) && isset($_SESSION['razina'])) echo '<li><a href="odjava.php">ODJAVA</a></li>';
             else echo '<li><a href="registracija.php">PRIJAVA</a></li>
         </ul>
